2f8094158464fbdd18950562ac12a2b004b31832
smarthjaswal <smarthjaswal3@gmail.com>
2023-02-28 18:47:17 +0530

    initials

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,7 +5,7 @@ import React, { useState } from 'react'
 
 export default function TextForm(props) {
     const handleUpClick = ()=>{
-    //   console.log("Uppercase was clicked"+ text);
+      // console.log("Uppercase was clicked"+ text);
       let newText = text.toUpperCase();
       setText(newText);
       
@@ -22,6 +22,15 @@ export default function TextForm(props) {
               setText(newText);
               
             }
+            const handleCopy =()=>{
+              var text = document.getElementById('myBox');
+              text.select();
+              navigator.clipboard.writeText(text.value);
+            }
+            const handleExtraSpaces = ()=>{
+              let newText = text.split(/[ ]+/);
+              setText(newText.join(" "))
+            }
     const handleOnChange = (event)=>{
         console.log("on change");
         setText(event.target.value);
@@ -33,11 +42,14 @@ export default function TextForm(props) {
         <div className='container'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange = {handleOnChange}id="exampleFormControlTextarea1" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange = {handleOnChange}id="myBox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
             <button className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to LowerCase</button>
             <button className="btn btn-primary mx-2" onClick={handleClearClick}>Clear Text</button>
+            <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
+            <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove Extra Space</button>
+
         </div>
         <div className='container my-3'>
          <h2>Your text summary</h2>
